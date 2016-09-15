223ab96a77f3623cbec637f42058333a7db3a04c
moderateepheezy <moderateepheezy@gmail.com>
2016-09-15 15:51:30 +0100

    Initial Commit

---
+++ Grocery/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Grocery
+//
+//  Created by SimpuMind on 9/15/16.
+//  Copyright © 2016 SimpuMind. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
